9bac5dc206a544499a9549d4624f61d078228ee8
wafiben <wafibenjeddou@gmail.com>
2022-10-14 21:38:58 +0200

    validation input

--- file.js
+++ file.js
@@ -1,18 +1,31 @@
 let userName = document.querySelector("#userName");
 let email = document.querySelector("#mail");
-let passwordOfUser = document.querySelector("#password");
+let passwordOfUser = document.querySelector("#password-btn");
 let buttonShowPassword = document.querySelector("#show-btn");
+const buttonSubmit = document.querySelector("#submit-btn");
+
+const validation = [false, false, false];
+
+buttonSubmit.disabled = true;
 
 userName.addEventListener("change", function (event) {
   let nameOfUser = event.target.value;
   if (nameOfUser.length < 5) {
     alert("userName should be at least 5 caracter");
+  } else {
+    validation[0] = true;
+    validationForm();
+    toggleStyle();
   }
 });
 email.addEventListener("change", function (event) {
   let mail = event.target.value;
   if (!mail.includes("@")) {
     alert("fomr invalid of email");
+  } else {
+    validation[1] = true;
+    validationForm();
+    toggleStyle();
   }
 });
 passwordOfUser.addEventListener("change", function (event) {
@@ -22,6 +35,51 @@ passwordOfUser.addEventListener("change", function (event) {
     alert(
       "the first caracter of the password must be uppercase and the length"
     );
+  } else {
+    validation[2] = true;
+    validationForm();
+    toggleStyle();
   }
 });
-buttonShowPassword.addEventListener("click", function (event) {});
+
+buttonShowPassword.addEventListener("click", function (event) {
+  if (passwordOfUser.type === "password") {
+    passwordOfUser.type = "text";
+    buttonShowPassword.innerHTML = "hide password";
+  } else {
+    passwordOfUser.type = "password";
+    buttonShowPassword.innerHTML = "show password";
+  }
+});
+function validationForm() {
+  let sum = 0;
+  for (let i = 0; i < validation.length; i++) {
+    if (validation[i] === true) {
+      sum += 1;
+    }
+  }
+  if (sum === validation.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function toggleStyle() {
+  if (validationForm()) {
+    buttonSubmit.disabled = false;
+    buttonSubmit.classList.add("btn-success");
+  } else {
+    buttonSubmit.disabled = true;
+    buttonSubmit.classList.add("btn-primary");
+  }
+}
+
+function login() {
+  window.location.href = "home.html";
+}
+
+buttonSubmit.addEventListener("click", function (event) {
+  event.preventDefault();
+  login();
+});
